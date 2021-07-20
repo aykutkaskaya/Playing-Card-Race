a0634289703233a7f7b4c4ddf98e1033a62a9f22
Aykut KAŞKAYA <aykutkaskaya@gmail.com>
2021-07-20 09:08:19 +0300

    oynanabilir durumda

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 class card {
-    constructor(type, number, title, id) {
+    constructor(type, number, title, id, selected) {
         this.type = type;
         this.number = number;
         this.title = title;
         this.id = id;
+        this.selected = selected;
     }
 }
 
@@ -12,11 +13,6 @@ var secret_two;
 var secret_three;
 var secret_four;
 var secret_five;
-
-var Selected_Clubs = [];
-var Selected_Diamonds = [];
-var Selected_Hearts = [];
-var Selected_Spades = [];
 
 var clubs_index = 6;
 var spades_index = 6;
@@ -28,13 +24,17 @@ var Diamonds = [];
 var Hearts = [];
 var Spades = [];
 
+var Deck = [];
+
 var card_types = ["Clubs", "Diamonds", "Hearts", "Spades"];
 
 for (let index = 1; index <= 13; index++) {
     var t;
+    var check = false;
     switch (index) {
         case 1:
             t = "A"
+            check = true;
             break;
         case 11:
             t = "J"
@@ -50,73 +50,65 @@ for (let index = 1; index <= 13; index++) {
             t = index;
             break;
     }
-    Clubs.push(new card("Clubs", index, t, index));
-    Diamonds.push(new card("Diamonds", index, t, (index + 13)));
-    Spades.push(new card("Spades", index, t, (index + 26)));
-    Hearts.push(new card("Hearts", index, t, (index + 39)));
+    var tempCard = new card("Clubs", index, t, index, check);
+    Clubs.push(tempCard);
+    Deck.push(tempCard);
+
+    tempCard = new card("Diamonds", index, t, (index + 13), check);
+    Diamonds.push(tempCard);
+    Deck.push(tempCard);
+
+    tempCard = new card("Spades", index, t, (index + 26), check);
+    Spades.push(tempCard);
+    Deck.push(tempCard);
+
+
+    tempCard = new card("Hearts", index, t, (index + 39), check);
+    Hearts.push(tempCard);
+    Deck.push(tempCard);
 
 }
 
+
+
 $(document).ready(function () {
 
+    MessageBox = function MessageBox(title, text, icon, confirmButtonText) {
+        Swal.fire({
+            title: title,
+            text: text,
+            icon: icon,
+            confirmButtonText: confirmButtonText
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Stop_The_Game();
+            }
+        })
+    }
+
     YeniKart = function YeniKart() {
+        var myCard;
+        do {
+            var myCard = PickACard();
+        } while (myCard.selected == true);
 
-        var myCard = PickACard();
+        myCard.selected = true;
+        $("#remCard").text(Deck.filter(a => a.selected == false).length);
+
         var id = "cardView" + myCard.id;
-
         $("#drawer").removeClass("empty-card");
         $("#drawer").empty();
         $("#drawer").append($("#" + id).clone());
 
-        var rowID;
-        switch (myCard.type) {
-            case "Clubs":
-                rowID = "col-" + clubs_index + "-2";
-                clubs_index--;
-                var theCard = $("#" + rowID + " .card");
-                rowID = "col-" + clubs_index + "-2";
-                $("#" + rowID).removeClass("empty-card");
-                $("#" + ("col-" + (Number(clubs_index) + 1) + "-2")).addClass("empty-card");
+        PushCard(myCard);
 
-                $("#" + rowID).append(theCard);
-                break;
-            case "Diamonds":
-                rowID = "col-" + diamonds_index + "-4";
-                diamonds_index--;
-                var theCard = $("#" + rowID + " .card");
-                rowID = "col-" + diamonds_index + "-4";
-                $("#" + rowID).removeClass("empty-card");
-                $("#" + ("col-" + (Number(diamonds_index) + 1) + "-4")).addClass("empty-card");
-                $("#" + rowID).append(theCard);
-                break;
-            case "Spades":
-                rowID = "col-" + spades_index + "-1";
-                spades_index--;
-                var theCard = $("#" + rowID + " .card");
-                rowID = "col-" + spades_index + "-1";
-                $("#" + rowID).removeClass("empty-card");
-                $("#" + ("col-" + (Number(spades_index) + 1) + "-1")).addClass("empty-card");
-                $("#" + rowID).append(theCard);
-                break;
-            case "Hearts":
-                rowID = "col-" + hearts_index + "-3";
-                hearts_index--;
-                var theCard = $("#" + rowID + " .card");
-                rowID = "col-" + hearts_index + "-3";
-                $("#" + rowID).removeClass("empty-card");
-                $("#" + ("col-" + (Number(hearts_index) + 1) + "-3")).addClass("empty-card");
+        var maxindex = MaxIndex();
 
-                $("#" + rowID).append(theCard);
-                break;
-
-            default:
-                break;
-        }
-
-        if ($("#row-6").hasClass("last-row") && $("#row-6 .card-place .card-slot").length < 1) {
-            $("#col-6-5").empty();
+        if (maxindex == 5 && $("#col-5-5").hasClass("turned") == true) {
+            $("#col-5-5").empty();
             var tempCard = "cardView" + secret_five.id;;
-            $("#col-6-5").append($("#" + tempCard).clone());
+            $("#col-5-5").append($("#" + tempCard).clone());
+            $("#col-5-5").removeClass("turned");
 
             switch (secret_five.type) {
                 case "Clubs":
@@ -135,15 +127,14 @@ $(document).ready(function () {
                 default:
                     break;
             }
-
-            $("#row-6").removeClass("last-row");
-            $("#row-5").addClass("last-row");
         }
 
-        if ($("#row-5").hasClass("last-row") && $("#row-5 .card-place .card-slot").length < 1) {
-            $("#col-5-5").empty();
-            var tempCard = "cardView" + secret_five.id;;
-            $("#col-5-5").append($("#" + tempCard).clone());
+
+        if (maxindex == 4 && $("#col-4-5").hasClass("turned") == true) {
+            $("#col-4-5").empty();
+            var tempCard = "cardView" + secret_four.id;;
+            $("#col-4-5").append($("#" + tempCard).clone());
+            $("#col-4-5").removeClass("turned");
 
             switch (secret_four.type) {
                 case "Clubs":
@@ -163,14 +154,13 @@ $(document).ready(function () {
                     break;
             }
 
-            $("#row-5").removeClass("last-row");
-            $("#row-4").addClass("last-row");
         }
 
-        if ($("#row-4").hasClass("last-row") && $("#row-4 .card-place .card-slot").length < 1) {
-            $("#col-4-5").empty();
-            var tempCard = "cardView" + secret_four.id;;
-            $("#col-4-5").append($("#" + tempCard).clone());
+        if (maxindex == 3 && $("#col-3-5").hasClass("turned") == true) {
+            $("#col-3-5").empty();
+            var tempCard = "cardView" + secret_three.id;;
+            $("#col-3-5").append($("#" + tempCard).clone());
+            $("#col-3-5").removeClass("turned");
 
             switch (secret_three.type) {
                 case "Clubs":
@@ -189,15 +179,12 @@ $(document).ready(function () {
                 default:
                     break;
             }
-
-            $("#row-4").removeClass("last-row");
-            $("#row-3").addClass("last-row");
         }
-
-        if ($("#row-3").hasClass("last-row") && $("#row-3 .card-place .card-slot").length < 1) {
-            $("#col-3-5").empty();
-            var tempCard = "cardView" + secret_three.id;;
-            $("#col-3-5").append($("#" + tempCard).clone());
+        if (maxindex == 2 && $("#col-2-5").hasClass("turned") == true) {
+            $("#col-2-5 ").empty();
+            var tempCard = "cardView" + secret_two.id;;
+            $("#col-2-5 ").append($("#" + tempCard).clone());
+            $("#col-2-5").removeClass("turned");
 
             switch (secret_two.type) {
                 case "Clubs":
@@ -217,39 +204,12 @@ $(document).ready(function () {
                     break;
             }
 
-            $("#row-3").removeClass("last-row");
-            $("#row-2").addClass("last-row");
         }
-        if ($("#row-2").hasClass("last-row") && $("#row-2 .card-place .card-slot").length < 1) {
-            $("#col-2-5 ").empty();
-            var tempCard = "cardView" + secret_two.id;;
-            $("#col-2-5 ").append($("#" + tempCard).clone());
-
-            switch (secret_one.type) {
-                case "Clubs":
-                    PullBack("club_ace", 2);
-                    break;
-                case "Diamonds":
-                    PullBack("diamond_ace", 4);
-                    break;
-                case "Spades":
-                    PullBack("spade_ace", 1);
-                    break;
-                case "Hearts":
-                    PullBack("heart_ace", 3);
-                    break;
-
-                default:
-                    break;
-            }
-
-            $("#row-2").removeClass("last-row");
-            $("#row-1").addClass("last-row");
-        }
-        if ($("#row-1").hasClass("last-row") && $("#row-1 .card-place .card-slot").length < 1) {
+        if (maxindex == 1 && $("#col-1-5").hasClass("turned") == true) {
             $("#col-1-5 ").empty();
             var tempCard = "cardView" + secret_one.id;;
             $("#col-1-5 ").append($("#" + tempCard).clone());
+            $("#col-1-5").removeClass("turned");
 
             switch (secret_one.type) {
                 case "Clubs":
@@ -272,48 +232,106 @@ $(document).ready(function () {
         }
 
         if (clubs_index == 0 || spades_index == 0 || hearts_index == 0 || diamonds_index == 0) {
-            alert("oyun bitti");
+
+            if (clubs_index == 0) {
+                MessageBox("Yarış Bitti!", "Sinek Kazandı!", "success", "Kapat")
+            }
+
+            if (spades_index == 0) {
+                MessageBox("Yarış Bitti!", "Maça Kazandı!", "success", "Kapat")
+            }
+
+            if (hearts_index == 0) {
+                MessageBox("Yarış Bitti!", "Kupa Kazandı!", "success", "Kapat")
+            }
+
+            if (diamonds_index == 0) {
+                MessageBox("Yarış Bitti!", "Karo Kazandı!", "success", "Kapat")
+            }
         }
-    };
-
-    PullBack = function PullBack(theCard, index) {
-        var currentRow = $("#" + theCard).parent().attr("id").split("-")[1];
-        $("#col-" + (Number(currentRow) + 1) + "-" + index).removeClass("empty-card").append($("#" + theCard));
-        $("#" + ("col-" + (Number(currentRow)) + "-" + index)).addClass("empty-card");
 
     };
 
-    PickACard = function PickACard() {
-
-        const cardType = RandomNumber(1, 4);
-        var type = card_types[cardType - 1];
-        var cardNumber = RandomNumber(2, 12);
-        var id;
-        switch (type) {
+    PushCard = function PushCard(mycard) {
+        switch (mycard.type) {
             case "Clubs":
-                Selected_Clubs.push(cardNumber);
-                id = Number(cardNumber);
+                clubs_index--;
+                var theCard = $("#club_ace");
+                var currentRow = theCard.parent().attr("id").split("-")[1];
+                var nextRow = (Number(currentRow) - 1);
+                $("#" + ("col-" + nextRow + "-2")).removeClass("empty-card");
+                $("#" + ("col-" + currentRow + "-2")).addClass("empty-card");
+                $("#" + ("col-" + nextRow + "-2")).append(theCard);
                 break;
             case "Diamonds":
-                Selected_Diamonds.push(cardNumber);
-                id = (Number(cardNumber) + 13);
+                diamonds_index--;
+                var theCard = $("#diamond_ace");
+                var currentRow = theCard.parent().attr("id").split("-")[1];
+                var nextRow = (Number(currentRow) - 1);
+                $("#" + ("col-" + nextRow + "-4")).removeClass("empty-card");
+                $("#" + ("col-" + currentRow + "-4")).addClass("empty-card");
+                $("#" + ("col-" + nextRow + "-4")).append(theCard);
                 break;
             case "Spades":
-                Selected_Spades.push(cardNumber);
-                id = (Number(cardNumber) + 26);
+                spades_index--;
+                var theCard = $("#spade_ace");
+                var currentRow = theCard.parent().attr("id").split("-")[1];
+                var nextRow = (Number(currentRow) - 1);
+                $("#" + ("col-" + nextRow + "-1")).removeClass("empty-card");
+                $("#" + ("col-" + currentRow + "-1")).addClass("empty-card");
+                $("#" + ("col-" + nextRow + "-1")).append(theCard);
                 break;
             case "Hearts":
-                Selected_Hearts.push(cardNumber);
-                id = (Number(cardNumber) + 39);
+                hearts_index--;
+                var theCard = $("#heart_ace");
+                var currentRow = theCard.parent().attr("id").split("-")[1];
+                var nextRow = (Number(currentRow) - 1);
+                $("#" + ("col-" + nextRow + "-3")).removeClass("empty-card");
+                $("#" + ("col-" + currentRow + "-3")).addClass("empty-card");
+                $("#" + ("col-" + nextRow + "-3")).append(theCard);
                 break;
 
             default:
                 break;
         }
 
-        return new card(type, cardNumber, cardNumber, id);
+    };
+
+    PullBack = function PullBack(theCard, index) {
+        var currentRow = $("#" + theCard).parent().attr("id").split("-")[1];
+        var nextRow = Number(currentRow) + 1;
+        $("#col-" + nextRow + "-" + index).removeClass("empty-card");
+        $("#col-" + nextRow + "-" + index).append($("#" + theCard));
+        $("#" + ("col-" + currentRow + "-" + index)).addClass("empty-card");
+
+        switch (theCard) {
+            case "club_ace":
+                clubs_index++;
+                break;
+            case "diamond_ace":
+                diamonds_index++;
+                break;
+
+            case "spade_ace":
+                spades_index++;
+                break;
+
+            case "heart_ace":
+                hearts_index++;
+                break;
+
+            default:
+                break;
+        }
 
     };
+
+    PickACard = function PickACard() {
+        var cardNumber = RandomNumber(1, 52);
+        var myc = Deck.find(a => a.id == (Number(cardNumber)));
+
+        return myc;
+    }
 
     Stop_The_Game = function Stop_The_Game() {
         $("#btnKartCek").hide();
@@ -324,23 +342,38 @@ $(document).ready(function () {
         location.reload();
     };
 
+    sortByKeyDesc = function sortByKeyDesc(array, key) {
+        return array.sort(function (a, b) {
+            var x = a[key];
+            var y = b[key];
+            return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+        });
+    }
+    sortByKeyAsc = function sortByKeyAsc(array, key) {
+        return array.sort(function (a, b) {
+            var x = a[key];
+            var y = b[key];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+    }
+
     StartTheGame = function StartTheGame() {
 
-        for (let index = 0; index < 5; index++) {
+        for (let index = 1; index < 6; index++) {
             switch (index) {
-                case 0:
+                case 1:
                     secret_one = PickACard();
                     break;
-                case 1:
+                case 2:
                     secret_two = PickACard();
                     break;
-                case 2:
+                case 3:
                     secret_three = PickACard();
                     break;
-                case 3:
+                case 4:
                     secret_four = PickACard();
                     break;
-                case 4:
+                case 5:
                     secret_five = PickACard();
                     break;
                 default:
@@ -349,6 +382,9 @@ $(document).ready(function () {
 
         }
 
+        Deck = sortByKeyAsc(Deck, "id");
+        $("#remCard").text(Deck.filter(a => a.selected == false).length);
+
     };
 
     RandomNumber = function RandomNumber(start, stop) {
@@ -356,5 +392,13 @@ $(document).ready(function () {
     };
 
     StartTheGame();
+
+    MaxIndex = function MaxIndex() {
+        var myArray = [clubs_index, diamonds_index, hearts_index, spades_index];
+        return Math.max.apply(Math, myArray);
+    }
+
+
+
 
 });
